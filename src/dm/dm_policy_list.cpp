f2afa638d7e2fcf8e62cff383a90f5df4fe1a7c5
Dharmalakshmi Annamalai <Dharmalakshmi_Annamalai@comcast.com>
2026-09-16 11:50:48 +0000

    Null check

--- src/dm/dm_policy_list.cpp
+++ src/dm/dm_policy_list.cpp
@@ -236,6 +236,10 @@ void dm_policy_list_t::update_list(const dm_policy_t& policy, dm_orch_type_t op)
 
         case dm_orch_type_db_update:
             ppolicy = get_policy(key);
+            if (ppolicy == NULL) {
+                em_printfout("update_list: policy not found for update, key=%s", key);
+                break;
+            }
             memcpy(&ppolicy->m_policy, &policy.m_policy, sizeof(em_policy_t));
             break;
 
