4bede4bbefb726ff96f4e68411afa98913c145cb
Dharmalakshmi Annamalai <Dharmalakshmi_Annamalai@comcast.com>
2026-09-25 09:44:41 +0000

    testing

--- src/em/em.cpp
+++ src/em/em.cpp
@@ -2050,6 +2050,7 @@ short em_t::create_def_8021q_settings_policy_tlv(unsigned char *buff)
 {
     size_t len = 0;
     dm_easy_mesh_t *dm;
+    unsigned int i;
 
     if (get_current_cmd()->get_type() == em_cmd_type_set_policy) {
         dm = get_current_cmd()->get_data_model();
@@ -2057,9 +2058,8 @@ short em_t::create_def_8021q_settings_policy_tlv(unsigned char *buff)
         dm = get_data_model();
     }
 
-    for (dm_policy_t *policy = dm->m_policy_map ? static_cast<dm_policy_t *>(hash_map_get_first(dm->m_policy_map)) : NULL;
-         policy != NULL;
-         policy = static_cast<dm_policy_t *>(hash_map_get_next(dm->m_policy_map, policy))) {
+    for (i = 0; i < dm->get_num_policy(); i++) {
+        dm_policy_t *policy = &dm->m_policy[i];
         if (policy->m_policy.id.type != em_policy_id_type_default_8021q_settings) {
             continue;
         }
