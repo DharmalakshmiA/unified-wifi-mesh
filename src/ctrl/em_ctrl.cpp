1821b69afe4f236de7caa5f6c6ace4198dfbd0f6
Dharmalakshmi Annamalai <Dharmalakshmi_Annamalai@comcast.com>
2026-09-25 10:06:20 +0000

    testing

--- src/ctrl/em_ctrl.cpp
+++ src/ctrl/em_ctrl.cpp
@@ -2238,8 +2238,6 @@ em_t *em_ctrl_t::find_em_for_msg_type(unsigned char *data, unsigned int len, em_
 	    }
 
 	    break;
-        case em_msg_type_topo_resp:
-        case em_msg_type_channel_pref_rprt:
         case em_msg_type_channel_sel_rsp:
         case em_msg_type_op_channel_rprt:
             if (em_msg_t(data + (sizeof(em_raw_hdr_t) + sizeof(em_cmdu_t)),
